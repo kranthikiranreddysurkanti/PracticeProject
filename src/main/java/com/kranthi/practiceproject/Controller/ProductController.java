0ff643da5f2ad448653f91d262b681ef22b8fe77
VISHNU VARDHAN <vishnuvardhan@Mac.lan>
2026-07-30 20:50:25 +0530

    created the modelsof product and category

--- src/main/java/com/kranthi/practiceproject/Controller/ProductController.java
+++ src/main/java/com/kranthi/practiceproject/Controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.kranthi.practiceproject.Controller;
 
+import com.kranthi.practiceproject.models.Product;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
 
-    @RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
-    public String getProduct(@PathVariable("id") Long id){
-        return "In the get product API to get product with id = " + id;
+    public void createProduct(Product p){
+
+    }
+
+    public Product getProduct(Long id){
+        return null;
     }
 }
---
+++ src/main/java/com/kranthi/practiceproject/models/Product.java
@@ -0,0 +1,70 @@
+package com.kranthi.practiceproject.models;
+
+public class Product {
+    private int id;
+    private String title;
+    private String description;
+    private double price;
+    private String imageUrl;
+    private Category category;
+
+    public Product() {
+    }
+
+    public Product(int id, String title, String description, double price, String imageUrl, Category category) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
